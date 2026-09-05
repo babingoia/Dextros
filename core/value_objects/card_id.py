76a6c92cbbbd3b30a575f2cdd4fbb143acecfe19
babingoia <137658431+babingoia@users.noreply.github.com>
2026-09-04 21:21:19 -0400

    Teste do CI

--- core/value_objects/card_id.py
+++ core/value_objects/card_id.py
@@ -25,8 +25,6 @@ class CardID:
                 return cls._new()
             case UUID():
                 return cls(value)
-            case str():
-                return cls._from_string(value)
             case int():
                 return cls._from_int(value)
             case _:
